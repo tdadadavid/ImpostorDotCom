e0663fb27232303cd47e180ef14d6185b8d6f78c
tdadadavid <davidtofunmidada@gmail.com>
2022-06-24 02:50:10 +0100

    refactored password comparison logic

--- components/v1/Users/validators/login.js
+++ components/v1/Users/validators/login.js
@@ -3,7 +3,6 @@
 const {errorMessage, catchAsyncError } = require('../../../../utils')
 const { loginSchema } = require('../validationSchema');
 const Users = require("../model");
-const {compare} = require("bcrypt");
 
 const validateRequest = catchAsyncError(async (req, res, next) => {
     const { error } = await loginSchema.validate(req.body);
@@ -19,7 +18,7 @@ const validateUser = catchAsyncError(async (req, res, next)  =>{
     let user = await Users.findOne({ email });
     if (!user) return errorMessage(res, 404, "User is not authenticated");
 
-    const isValid = await compare(password, user.password);
+    const isValid = await user.comparePassword(password);
     if(!isValid) return errorMessage(res, 400, "Passwords don't match");
 
     req.user = user;
