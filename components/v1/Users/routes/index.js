6d6ab42857d3f2e1f3e2a8cf889a2735efc7478a
tdadadavid <davidtofunmidada@gmail.com>
2022-06-24 03:36:53 +0100

    refactored the middlewares for the login routes

--- components/v1/Users/routes/index.js
+++ components/v1/Users/routes/index.js
@@ -1,12 +1,14 @@
-const UserController = require('../controllers')
-const { Router } = require('express');
-const {validateBody, ensureUniqueness, validateRequest, validateUser} = require("../validators");
+"use strict";
 
+const UserController = require('../controllers')
+const {validateBody, ensureUniqueness, validateRequest, authUser, confirmPassword} = require("../validators");
+
+const { Router } = require('express');
 const userRouter = Router();
 
-
-userRouter.post('/', [validateBody, ensureUniqueness], UserController.signUp)
-userRouter.post('/login', [validateRequest, validateUser], UserController.login);
+userRouter.post('/', [validateBody, ensureUniqueness], UserController.signUp);
+userRouter.post('/login', [validateRequest, authUser, confirmPassword], UserController.login);
+// userRouter.post('/forgotPassword', [validateEmail, ])
 
 
 module.exports = userRouter;
