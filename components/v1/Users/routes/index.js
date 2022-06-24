68fd00d75be35bb3aea5bb043933e05594bececf
tdadadavid <davidtofunmidada@gmail.com>
2022-06-24 03:52:29 +0100

    add schema validation for password reset request

--- components/v1/Users/routes/index.js
+++ components/v1/Users/routes/index.js
@@ -1,14 +1,14 @@
 "use strict";
 
 const UserController = require('../controllers')
-const {validateBody, ensureUniqueness, validateRequest, authUser, confirmPassword} = require("../validators");
+const { validateBody, ensureUniqueness, validateRequest, authUser, confirmPassword, passwordResetValidator} = require("../validators");
 
 const { Router } = require('express');
 const userRouter = Router();
 
 userRouter.post('/', [validateBody, ensureUniqueness], UserController.signUp);
 userRouter.post('/login', [validateRequest, authUser, confirmPassword], UserController.login);
-// userRouter.post('/forgotPassword', [validateEmail, ])
+userRouter.post('/forgotPassword', [ passwordResetValidator ]);
 
 
 module.exports = userRouter;
