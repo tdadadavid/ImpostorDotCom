01310c4d049cbcc35680fb36b74d7c8d8c6cff69
tdadadavid <davidtofunmidada@gmail.com>
2022-06-27 02:04:53 +0100

    add more tests that pass

--- src/components/v1/Users/__tests__/integration/reset-password.test.js
+++ src/components/v1/Users/__tests__/integration/reset-password.test.js
@@ -1,71 +1,71 @@
 const request = require('supertest');
-const app = require('../../../../../app');
+const app = require('../../../../../../app');
 const setup = require('../index');
 const Users = require('../../model');
 
 
 setup();
 
-// describe('Request for password reset', () => {
-//     /***
-//      * Expectations:
-//      *
-//      * return 400 if email is not given
-//      * return 404 if the email doesn't exist
-//      * return 200 if request is valid
-//      *
-//      */
-//
-//     let response;
-//     let payload = { email: "dummy@gmail.com" };
-//     const makePostRequest = () => {
-//         return request(app)
-//             .post('/api/auth/users/forgot-password')
-//             .send(payload)
-//     }
-//     let user = {
-//         firstname: "King",
-//         lastname: "Mick",
-//         username: "mickJod",
-//         email: "dummy@gmail.com",
-//         phone: "+23481823939393",
-//         password: "AWbn09890@#"
-//     };
-//
-//     beforeEach(async () => {
-//        payload = { email: "dummy@gmail.com" };
-//        await Users.create(user);
-//     });
-//
-//     afterEach(async () => {
-//         await Users.deleteOne({ email: "dummy@gmail.com" });
-//     })
-//
-//     it('should return 400 if email is not given', async () => {
-//         delete payload.email;
-//
-//         response = await makePostRequest();
-//
-//         expect(response.status).toEqual(400);
-//         expect(response.body.message).toContain('email');
-//     });
-//
-//     it('should return 404 if the user is not authenticated', async () => {
-//         payload.email = "unauthenticated@gmail.com";
-//
-//         response = await makePostRequest();
-//
-//         expect(response.status).toBe(404);
-//         expect(response.body.message).toContain('not authenticated');
-//     });
-//
-//     it('should return 200 if the request is valid', async () => {
-//         response = await makePostRequest();
-//
-//         expect(response.status).toBe(200);
-//         expect(response.body.message).toContain('email');
-//     });
-// });
+describe('Request for password reset', () => {
+    /***
+     * Expectations:
+     *
+     * return 400 if email is not given
+     * return 404 if the email doesn't exist
+     * return 200 if request is valid
+     *
+     */
+
+    let response;
+    let payload = { email: "dummy@gmail.com" };
+    const makePostRequest = () => {
+        return request(app)
+            .post('/api/auth/users/forgot-password')
+            .send(payload)
+    }
+    let user = {
+        firstname: "King",
+        lastname: "Mick",
+        username: "mickJod",
+        email: "dummy@gmail.com",
+        phone: "+23481823939393",
+        password: "AWbn09890@#"
+    };
+
+    beforeEach(async () => {
+       payload = { email: "dummy@gmail.com" };
+       await Users.create(user);
+    });
+
+    afterEach(async () => {
+        await Users.deleteOne({ email: "dummy@gmail.com" });
+    })
+
+    it('should return 400 if email is not given', async () => {
+        delete payload.email;
+
+        response = await makePostRequest();
+
+        expect(response.status).toEqual(400);
+        expect(response.body.message).toContain('email');
+    });
+
+    it('should return 404 if the user is not authenticated', async () => {
+        payload.email = "unauthenticated@gmail.com";
+
+        response = await makePostRequest();
+
+        expect(response.status).toBe(404);
+        expect(response.body.message).toContain('not authenticated');
+    });
+
+    it('should return 200 if the request is valid', async () => {
+        response = await makePostRequest();
+
+        expect(response.status).toBe(200);
+        expect(response.body.message).toContain('email');
+    });
+});
 
 describe('Reset user password', () => {
 
@@ -83,28 +83,29 @@ describe('Reset user password', () => {
      let response;
      let payload = { password: "29zmqAX10#@", confirm_password: "29zmqAX10#@" };
      let token;
+
      const makePostRequest = () => {
          return request(app)
              .post(`/api/auth/users/resets?access_token=Bearer ${token}`)
              .send(payload)
      }
-     let user = {
-         firstname: "King",
-         lastname: "Mick",
-         username: "mickJod",
-         email: "dummy@gmail.com",
-         phone: "+23481823939393",
-         password: "AWbn09890@#"
-     };
-
-     beforeEach(async () => {
+     const makeUser = async () => {
+         let user = {
+             firstname: "King",
+             lastname: "Mick",
+             username: "mickJod",
+             email: "dummy@gmail.com",
+             phone: "+23481823939393",
+             password: "AWbn09890@#"
+         };
          user = await Users.create(user);
          token = user.generatePasswordResetToken();
-     });
+     }
 
      afterEach(async () => {
          await Users.deleteOne({ email: "dummy@gmail.com" });
-     });
+     })
+
 
     it('should return 422 error if no token is provided', async () => {
         token = "";
@@ -115,6 +116,53 @@ describe('Reset user password', () => {
 
     it('should return 403 if token is invalid', async () => {
         token = "invalidToken";
+
+        response = await makePostRequest();
+        expect(response.body.message).toContain("Invalid");
+        expect(response.status).toBe(403);
     });
+
+    it('should return 422 if user is not authenticated', async () => {
+        const user_to_be_deleted = await Users({
+            firstname: "deleted_user",
+            lastname: "deleted_user",
+            username: "deleted",
+            email: "deleted@gmail.com",
+            phone: "+23481823933",
+            password: "AWbn09890@#"
+        });
+
+        user_to_be_deleted.save();
+
+        token = user_to_be_deleted.generatePasswordResetToken();
+
+        await Users.deleteOne({ email: "deleted@gmail.com" });
+
+        response = await makePostRequest();
+        expect(response.status).toEqual(422);
+        expect(response.body.message).toContain("authenticated");
+    });
+
+    it('should return 400 if the password is not given', async () => {
+        await makeUser();
+
+        delete payload.password;
+
+        response = await makePostRequest();
+        expect(response.status).toBe(400);
+        expect(response.body.message).toContain('password');
+    });
+
+    it('should return 400 if the confirm password is not given', async () => {
+        await makeUser();
+
+        delete payload.confirm_password;
+
+        response = await makePostRequest();
+        expect(response.status).toBe(400);
+        expect(response.body.message).toContain('password');
+    });
+
+
 
 });
