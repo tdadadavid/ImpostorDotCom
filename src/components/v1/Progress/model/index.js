d7f397040b358d2db7723301c7ae7d807c784aab
tdadadavid <davidtofunmidada@gmail.com>
2022-06-29 05:48:06 +0100

    add progress schema and model

---
+++ src/components/v1/Progress/model/index.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const {Schema, model} = require('mongoose');
+
+const progressSchema = new Schema({
+
+   body: {
+       type: String,
+       required: false,
+       minLength: 1,
+       maxLength: 255
+   },
+
+   image_path: {
+       type: String,
+       required: false,
+       default: "", // will be implemented later
+   },
+
+   tags: {
+       type: [ String ],
+       required: false,
+       default: [
+           "#progress",
+           "#consitency",
+           "#self-celebration"
+       ],
+   },
+
+   visibility: {
+       type: String,
+       required: true,
+       default: "Private"
+   }
+
+});
+
+const Progress = model('Progress', progressSchema);
+
+module.exports = Progress;
